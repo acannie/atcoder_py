ff3f11255b9a9da19c0db1f3fbaa783dcd92def8
acannie <acannie0530@gmail.com>
2022-02-18 03:38:43 +0900

    code: chmin, chmax を実装。

--- src/others/dp_matome_contest/a.py
+++ src/others/dp_matome_contest/a.py
@@ -1,4 +1,18 @@
-def main():
+def chmin(dp, i, candidate):
+    if dp[i] > candidate:
+        dp[i] = candidate
+        return True
+    return False
+
+
+def chmax(dp, i, candidate):
+    if dp[i] < candidate:
+        dp[i] = candidate
+        return True
+    return False
+
+
+def fun1():
     # s1 = input().split()
     h = [2, 9, 4, 5, 1, 6, 10]
 
@@ -21,6 +35,35 @@ def main():
         dp.append(d_new)
 
     print(dp)
+
+
+def fun2():
+    # s1 = input().split()
+    h = [2, 9, 4, 5, 1, 6, 10]
+
+    # 無限大で初期化
+    dp = []
+    for i in range(len(h)):
+        dp.append(float('inf'))
+
+    dp[0] = 0
+    dp[1] = abs(h[0] - h[1])
+
+    for i in range(len(h)):
+        if i == 0 or i == 1:
+            continue
+        d1 = abs(h[i]-h[i-1])
+        d2 = abs(h[i]-h[i-2])
+
+        chmin(dp, i, d1 + dp[i-1])
+        chmin(dp, i, d2 + dp[i-2])
+
+    print(dp)
+
+
+def main():
+    fun1()
+    fun2()
 
 
 if __name__ == "__main__":
